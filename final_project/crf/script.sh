2e65e083e4f5c344f4184a96adb1053076931fa1
Alessia <alessiatovo@users.noreply.github.com>
2017-06-28 10:05:06 +0200

    Fixed bug

--- final_project/crf/script.sh
+++ final_project/crf/script.sh
@@ -3,5 +3,5 @@
 ./merge.py 3
 crf_learn template/t_008 data/merged_train.txt model/model_008
 crf_test -m model/model_008 data/merged_test.txt > output/output_008
-perl conlleval.pl -d '\t' < output/output_01_compl > eval/eval_008
+perl conlleval.pl -d '\t' < output/output_008 > eval/eval_008
 cat eval/eval_008
